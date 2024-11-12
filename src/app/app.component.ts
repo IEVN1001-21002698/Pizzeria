0e0d575b88e20a9c66582ce0ab3c1f0fc9d56430
Omar Avelar Nachez <raven.reaper0601@gmail.com>
2024-11-11 19:30:11 -0600

    FinalRevisado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from "./tienda/formulario/formulario.component";
+import { ClientesComponent } from "./tienda/clientes/clientes.component";
+import { VentasComponent } from "./tienda/ventas/ventas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormularioComponent, ClientesComponent, VentasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
